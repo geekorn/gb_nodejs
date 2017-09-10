f534af12db3df443459c8fa8730794fafa4aa0f3
Serj <serj.korneev@gmail.com>
2017-09-10 19:07:53 +0300

    refactoring code, add http server, add output ot browser on localhost:8080

--- news.js
+++ news.js
@@ -1,25 +1,46 @@
+const http = require('http');
 const request = require('request');
 const cheerio = require('cheerio');
 
-const url = 'https://habrahabr.ru/';
+const PORT = 8080;
+const URL = 'https://habrahabr.ru/';
+let posts = [];
 
-request(url, (error, response, body) => {
-    if (error) console.dir(err);
-    if (response.statusCode >= 400) console.log(response.statusCode);
+request(URL, (error, response, html) => {
+    if (error) {
+        return console.error('error is: ', error);
+    }
 
-    const $ = cheerio.load(body);
-    let posts = [];
+    if (response.statusCode !== 200) {
+        return console.log('incorrect statusCode: ', response.statusCode);
+    }
+
+    const $ = cheerio.load(html);
 
     $('.post').each( (i, item) => {
-        let post = {};
+        let post = {
+            toString: function () {
+                return `${this.time} | ${this.author} | <a href="${this.link}">${this.title}</a>`
+            }
+        };
 
         post.author = $(item).find('.user-info__nickname').text();
         post.title = $(item).find('.post__title_link').text();
         post.link = $(item).find('.post__title_link').attr('href');
         post.time = $(item).find('.post__time').text();
 
-        posts.push(post)
+        posts.push(post.toString())
     })
-
-    console.dir(posts)
 })
+
+
+http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html;  charset=utf-8'
+    });
+
+    response.write(posts.join('<br/>'));
+    response.end();
+}).listen(PORT, function () {
+    console.log("Let's get started: http://localhost:" + PORT);
+});
